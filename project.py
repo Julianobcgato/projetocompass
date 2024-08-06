17eb557c62d68e41d392d3419515d120d58b8a2f
Julianobcgato <155676559+Julianobcgato@users.noreply.github.com>
2024-08-06 08:41:19 -0300

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Mario": "World"}
+    return {"Luigi": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
